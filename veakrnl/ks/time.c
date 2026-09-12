27ea8bc861a23f53d70ae8df301f6fa7ae727934
RasyaFr-Engkol <rasya.fadhillah126@smk.belajar.id>
2026-09-12 18:43:52 +0700

    VEAOS:KS | Adding time.c as part for kernel-time-handling. Also testing SPDX

---
+++ veakrnl/ks/time.c
@@ -0,0 +1,17 @@
+/* VeaOS SPDX License ----------------------------------------------------
+    SPDX-License-Identifier: GPL-2.0-only
+    LICENSE     : GNU General Public License v2.0
+    FILE        : time.c
+    CREATOR     : RasyaFR-Engkol
+    MAINTAINER  : VeaOS Team
+    PURPOSE     : Part of KS used to handling system time and another related
+                  to time system
+----- Effective since 07-2026 til FOREVER ------------------------------- */
+
+#include <veakrnl.h>
+
+/* Revision History ------------------------------------------------------
+    * DATE          : 26-07-2026
+    * AUTHOR        : Cool Dev
+    * REVISION      : Added resetting TPR to IRQL PASSIVE_LEVEL
+* --------------------------------------------------------------------- */
