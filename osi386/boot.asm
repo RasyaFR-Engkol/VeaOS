c26d3f8f7f84f6b5364ba840fa5e0f6c94027e05
RasyaFr-Engkol <rasya.fadhillah126@smk.belajar.id>
2026-09-13 06:13:58 +0700

    VEAOS:MBR | Adding new GDT and some bug fix

--- osi386/boot.asm
+++ osi386/boot.asm
@@ -6,27 +6,32 @@ extern main
 _start:
     cli
 
-    ; Setup DS = 0 biar CPU bisa baca memori pakai absolute address > 0xFFFF
-    xor ax, ax
+    mov ax, 0x1000
     mov ds, ax
 
+    ; Enable A20 Gate (Fast A20)
     in al, 0x92
     or al, 2
     out 0x92, al
 
-    ; PAKSA 32-bit relocation pakai dword
-    lgdt [dword gdt_descriptor]
+    ; Ambil alamat absolut gdt_descriptor (misal: 0x10050)
+    ; lalu kurangi 0x10000 untuk mendapatkan offset lokal (misal: 0x0050)
+    mov ebx, gdt_descriptor
+    sub ebx, 0x10000
 
+    ; Load GDT menggunakan register BX (16-bit)
+    ; DS:BX = 0x1000:0x0050 -> Linear 0x10050. 
+    ; Nilai BX dijamin < 0xFFFF, jadi aman dari GPF!
+    lgdt [bx]
+
+    ; Aktifkan Protected Mode
     mov eax, cr0
-    or eax, 1        ; pake eax lebih aman dari al
+    or eax, 1
     mov cr0, eax
 
-    ; PAKSA 32-bit offset jump
+    ; Far jump ke 32-bit (Otomatis limit berpindah jadi 4GB)
     jmp dword 0x08:init_32bit
 
-; ==========================================
-; GLOBAL DESCRIPTOR TABLE (GDT)
-; ==========================================
 align 4
 gdt_start:
 
@@ -65,6 +70,12 @@ gdt_data16:         ; Entry 5: Data 16-bit
     db 10010010b    
     db 00001111b    
     db 0x00
+
+gdt_reserved:       ; Entry 5: Reserved / User (0x28)
+    dq 0            ; Slot kosong 8-byte
+
+gdt_pcr:            ; Entry 6: KPCR / FS Register (0x30)
+    dq 0            ; Nanti bakal di-overwrite oleh LdrSetFsSegmentBase()
 
 gdt_end:
 
