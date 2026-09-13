d2e7bff8ab893aea0d14120fbe7a5bac8ec84b9d
RasyaFr-Engkol <rasya.fadhillah126@smk.belajar.id>
2026-09-13 08:29:22 +0700

    VEAOS:KS | Modified the VeaOS startup process using several references from ReactOS.

--- veakrnl/ks/krnlinit.c
+++ veakrnl/ks/krnlinit.c
@@ -3,55 +3,110 @@
 #include <veakrnl.h>
 
 ULONG ks_cpu_count = 0;
-#define MAX_KERNEL_SIZE
-ULONG ks_kernel_stack[0x2000];
 
-VOID ks_system_startup_boot_stack(PBLOCK_BOOT_1 block_boot);
+VOID ks_system_startup_boot_stack(PBLOCK_BOOT_2 block_boot);
+PBLOCK_BOOT_2 KsLoaderBlock = NULL;
+KPRCB KsProcessorBlock[MAX_CPU];
 
-VOID ks_system_startup(PBLOCK_BOOT_1 block_boot)
+VOID
+VEAPI
+KiInitSystem(VOID)
+{
+    PKPRCB CurrentPrcb = KeGetCurrentPrcb();
+    /* Initialize Timer List */
+    KsiInitializeTimerList();
+
+    /* Initialize it's DPC Timer and set it as first DPC to ever run */
+    KsInitializeDpc(&KiTimerExpireDpc, KsiTimerExpirationDpcRoutine, NULL);
+
+    /* DPC Stack is done allocated by our loader. Init LOCK and linked lsit */
+    KsInitializeSpinlock(&CurrentPrcb->DpcLock);
+    InitializeListHead(&CurrentPrcb->DpcListHead);
+}
+
+VOID
+VEAPI
+KsiInitializeKernel(
+    IN PKPROCESS InitProcess,
+    IN PKTHREAD InitThread,
+    IN PVOID IdleStack,
+    IN PKPRCB Prcb,
+    IN PBLOCK_BOOT_2 BlockBoot
+)
+{
+    PVOID TrueStackBase = (PVOID)((ULONG_PTR)IdleStack - 8192);
+
+    // Init hand built thread
+    PtInitializeHandBuildThread(InitThread, InitProcess, TrueStackBase, 8192, NULL);
+
+    // Daftarkan ke PRCB dan RunQueue
+    Prcb->CurrentThread = (PKTHREAD)InitThread;
+    PtRunQueue[Prcb->Number].CurrentThread = InitThread;
+
+    // Init Upper Layer
+    ul_initialize_layer(BlockBoot);
+}
+
+VOID ks_system_startup(PBLOCK_BOOT_2 block_boot)
 {
     ULONG cpu;
-    ULONG_PTR stack_top;
+    ULONG_PTR InitialStack = block_boot->KernelStack;
+    ULONG_PTR StackTop;
+
+    asm volatile("cli");
 
     /* naikan jumlah cpu */
     cpu = ks_cpu_count++;
 
+    /* Save LoaderBlock */
+    KsLoaderBlock = block_boot;
+
     /* kalo cpu 0, aktifkan serial aja */
     if(!cpu)
     {
-        kdp_activate_serial();
+        // Set our FS again to KGDT32_R0_PCR
+        KeReloadFsPcr();
 
+        kdp_activate_serial();
         kdp_print("Hello from ks_system_startup.\n\r");
+
+        KiInitSystem();
 
         ks_initialize_exception();
         kdp_print("Initialize exception done.\n\r");
     }
-
-    /* jump to kernel stack */
-    stack_top = (ULONG_PTR)ks_kernel_stack + sizeof(ks_kernel_stack);
-    ksi_switch_stack(stack_top, (PVOID)ks_system_startup_boot_stack, (PVOID)block_boot);
-}
-
-VOID 
-VEAPI 
-UlDumpPoolTracker(VOID);
-
-VOID ks_system_startup_boot_stack(PBLOCK_BOOT_1 block_boot)
-{
-
-    ul_initialize_layer(block_boot);
-
+    
     HctInitializeProcessor(block_boot, ks_cpu_count - 1);
 
-    ANSI_STRING StringThis;
-    RtlInitAnsiString(&StringThis, "/SomeFunckingNugger");
+    /* jump to kernel stack */
+    StackTop = (ULONG_PTR)InitialStack;
+    ksi_switch_stack(StackTop, (PVOID)ks_system_startup_boot_stack, (PVOID)block_boot);
+}
 
-    OBJECT_ATTRIBUTES ObjAttr;
-    InitializeObjectAttributes(&ObjAttr, &StringThis, OBJ_CASE_INSENSITIVE, NULL, NULL);
-    VeaCreateDirectoryNamespace(&ObjAttr);
+VOID ks_system_startup_boot_stack(PBLOCK_BOOT_2 block_boot)
+{
+    PKPRCB Prcb = KeGetCurrentPrcb();
+    PKTHREAD Thread = (PKTHREAD)block_boot->Thread;
+    PKPROCESS Process = (PKPROCESS)Thread->ApcState.Process;
+    PVOID KernelStack = (PVOID)block_boot->KernelStack;
 
-    ObDumpObjectTree();
+    if(Prcb->Number == 0)
+    {
+        KsiInitializeKernel(
+            Process, 
+            Thread, 
+            KernelStack, 
+            Prcb, 
+            block_boot
+        );
+    }
+    else
+    {
+        PtInitializeHandBuildThread(Thread, Process, KernelStack, 8192, NULL);
+    }
+    
+    asm volatile("sti");
+    KeLowerIrql(PASSIVE_LEVEL);
 
-    // todo: buat task baru untuk muter muter
-    while(1) asm("hlt");
+    PtIdleLoop();
 }
