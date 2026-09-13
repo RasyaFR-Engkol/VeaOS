92dbb07244a2887a75ac6378f6e95693d3ed4327
RasyaFr-Engkol <rasya.fadhillah126@smk.belajar.id>
2026-09-13 08:31:01 +0700

    VEAOS:BUG | More expressive exception.

--- veakrnl/ks/x86/exception.c
+++ veakrnl/ks/x86/exception.c
@@ -21,14 +21,24 @@ ks_setup_idt_trap(ULONG Vector, PVOID Routine, ULONG CS, ULONG Flags)
 
 VOID
 VEAPI
+KsRegisterSystemInterruptHandler(
+    PVOID Handler,
+    ULONG Vector
+)
+{
+    ks_setup_idt_trap(Vector, Handler, KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
+}
+
+VOID
+VEAPI
 ks_initialize_exception(VOID)
 {
     ks_setup_idt_trap(0,  (PVOID)isr_0_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
     ks_setup_idt_trap(1,  (PVOID)isr_1_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
     ks_setup_idt_trap(2,  (PVOID)isr_2_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
+    ks_setup_idt_trap(3,  (PVOID)isr_3_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
     ks_setup_idt_trap(6,  (PVOID)isr_6_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
     ks_setup_idt_trap(8,  (PVOID)isr_8_entry,  KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
-    // 2. Setup GDT & Memory Fault yang krusial untuk kestabilan kernel
     ks_setup_idt_trap(13, (PVOID)isr_13_entry, KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
     ks_setup_idt_trap(14, (PVOID)isr_14_entry, KGDT32_R0_CODE, IDT_FLAGS_INTR_RING0);
 
@@ -54,18 +64,34 @@ ksi_dispatch_exception(PKREGISTER_FRAME register_frame)
             :             // Input operand (kosong)
             :             // Clobbered registers (kosong)
         );
-        // Success = MmHandlePageFault(Cr2);
 
-        if(!Success)
+        BOOLEAN WriteFault = (register_frame->ErrorCode & 2) != 0;
+        BOOLEAN WasPresent = (register_frame->ErrorCode & 1) != 0;
+        
+        if (MmAccessFault(Cr2, WriteFault, WasPresent))
         {
-            KsBugCheck(0x50, (ULONG_PTR)Cr2, register_frame->Vector, register_frame->ErrorCode, 0);
+            return; // fault ke-resolve, balik lanjut eksekusi normal (IRET)
         }
+
+        KsBugCheckEx(0x50, (ULONG_PTR)Cr2, register_frame->Vector, register_frame->ErrorCode, 0);
+        return; // jaga-jaga kalau KsBugCheckEx ternyata bisa return
     }
     else if(register_frame->Vector == KS_EXCEPTION_GENERAL_PROTECTION && (register_frame->Cs & RPL_MASK) == MODE_USER)
     {
         // handle GP caused by user
     }
+    /* Debug Breakpoint or Debug by CPU */
+    else if (register_frame->Vector == KS_EXCEPTION_BREAKPOINT)
+    {
+        /* Give this to Kdp */
+        BOOLEAN Handled = KdpHandleDebug(register_frame);
+        if (Handled)
+        {
+            return;
+        }
+    }
 
-    kdp_print("ERROR. Something Happened.\n\r");
+    /* Exception isn't handled */
+    KsBugCheckEx(KMODE_EXCEPTION_NOT_HANDLED, register_frame->Vector, register_frame->ErrorCode, 0, 0);
     while(1) asm("hlt");
 }
